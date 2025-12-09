5c8237d781cbc7470a2ad34fb00032f22698eb05
FernandezMische <cheelizafernandez999@gmail.com>
2025-12-09 12:51:25 +0200

    updated DashboardPage

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -125,13 +125,7 @@ onMounted(async () => {
         </div>
       </div>
 
-      <div class="card text-end" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Special</h5>
-          <p class="card-text">Extra content</p>
-          <a href="#" class="btn btn-primary">Go</a>
-        </div>
-      </div>
+     
     </div>
     
     
