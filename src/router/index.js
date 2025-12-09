d01240c667db73ddef92920bc9bdeda745197e9d
marcofisher21-svg <marcofisher21@gmail.com>
2025-12-09 15:42:26 +0200

    added imgs

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '../views/DashboardPage.vue'
 import loginPage from '../views/loginPage.vue'
 import AttendancePage from '../views/attendancePage.vue'
-import PayrollPage from '../views/PayRollPage.vue'
+import PayrollPage from '../views/PayrollPage.vue'
 import EmployeePage from '../views/employeePage.vue'
 
 
