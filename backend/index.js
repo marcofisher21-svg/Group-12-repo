9961145d781819f44c2a5d4f2755aeca33c5e8b3
FernandezMische <cheelizafernandez999@gmail.com>
2026-02-04 12:17:26 +0200

    mische update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // index.js
 import express from 'express';
+import cors from 'cors'; 
 import {
   getEmployees,
   postEmployee,
@@ -8,6 +9,7 @@ import {
 } from './controllers/employeeController.js';
 
 const app = express();
+app.use(cors()); 
 app.use(express.json());
 
 app.get('/employee', getEmployees);
