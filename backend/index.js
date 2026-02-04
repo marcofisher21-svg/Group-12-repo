bd88b93f3012e91fe39d814efe8f94dc04d0dabf
Yaqoob-Samsodien <yaqoobsamsodien01@gmail.com>
2026-02-04 16:03:41 +0200

    created leave

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ import {
 
 import { deleteAttendance, getAttendance, postAttendance , patchAttendance} from './controllers/attendanceController.js';
 
+import { getLeave, postLeave, patchLeave, deleteLeave} from './controllers/leaveController.js';
+
 const app = express();
 app.use(cors()); 
 app.use(express.json());
@@ -23,6 +25,11 @@ app.get('/attendance', getAttendance);
 app.post('/attendance', postAttendance);
 app.patch('/attendance/:attendancedID', patchAttendance);
 app.delete('/attendance/:attendancedID', deleteAttendance);
+
+app.get('/leave', getLeave);
+app.post('/leave',postLeave);
+app.patch('/leave/:leaveRequest_ID',patchLeave);
+app.delete('/leave/:leaveRequest_ID',deleteLeave);
 
 app.listen(2006, () => {
   console.log('http://localhost:2006');
